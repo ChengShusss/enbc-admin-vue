e5611e35927f24973e8bb959687f2b76d70712e8
chengshu02 <chengshu02@meituan.com>
2021-07-21 12:08:11 +0800

    add v-charts

--- src/main.js
+++ src/main.js
@@ -12,6 +12,24 @@ import router from './router'
 import { menuHeader, menuAside } from '@/menu'
 import { frameInRoutes } from '@/router/routes'
 
+import VeLine from 'v-charts/lib/line.common'
+import VeMap from 'v-charts/lib/map.common'
+// 下面的依赖可以按照需求选择性加载
+// The following dependencies can be selectively loaded on demand
+// import 'echarts/lib/component/markLine'
+// import 'echarts/lib/component/markPoint'
+// import 'echarts/lib/component/markArea'
+// import 'echarts/lib/component/visualMap'
+// import 'echarts/lib/component/dataZoom'
+// import 'echarts/lib/component/toolbox'
+// import 'echarts/lib/component/title'
+// import 'zrender/lib/svg/svg'
+// import 'v-charts/lib/style.css'
+
+[VeLine, VeMap].forEach(comp => {
+  Vue.component(comp.name, comp)
+})
+
 // 核心插件
 Vue.use(d2Admin)
 
